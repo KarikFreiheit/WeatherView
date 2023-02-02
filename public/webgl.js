2fbb93fcc4aff42794f99bb301d6ef864492aa47
KarikFreiheit <crossoverkarik@gmail.com>
2023-02-02 10:28:39 -0800

    Repositioned Canvas

--- public/webgl.js
+++ public/webgl.js
@@ -1,9 +1,14 @@
 var canvas = document.getElementById("canvas")
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 var gl = canvas.getContext("webgl2")
+
 
 if(!gl){
     alert("Your browser does not support webgl")
 }
 
+//Sets background of canvas
 gl.clearColor(.5, .5, 1, 1)
 gl.clear(gl.COLOR_BUFFER_BIT)
