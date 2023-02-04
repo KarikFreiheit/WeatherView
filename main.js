140928494eb69fbfd52b9eca04965762076b1c49
KarikFreiheit <crossoverkarik@gmail.com>
2023-02-03 16:19:04 -0800

    Start working on json

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+
+app.use(express.static('public'), express.json())
 
 app.get('/', function(request, response){
-    response.sendFile('public');
+    response.sendFile('public')
+    response.send('vectors.json')
+});
+
+app.get('/json', function(request, response){
+    response.sendFile('./vectors.json')
 });
 
 const port = 3000;
+
+app.post('/', function(request, response){
+    const body = req.body;
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
--- public/webgl.js
+++ public/webgl.js
@@ -1,6 +1,11 @@
 var canvas = document.getElementById("canvas")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+data = fetch("./vectors.json").then((data) => {
+    console.log(data)
+})
+console.log(data[0])
+
 
 
 var gl = canvas.getContext("webgl2")
@@ -11,11 +16,14 @@ if(!gl){
 }
 
 
+
 //Creates and compiles vertex and fragment shaders
 const vertexShaderSource = `
+attribute vec2 position;
 void main(){
-    gl_Position = vec4(0,0,0,1);
-    gl_PointSize = 50.0;
+    
+    gl_Position = vec4(position, 0, 1);
+    gl_PointSize = 10.0;
 }`
 const vs = gl.createShader(gl.VERTEX_SHADER)
 gl.shaderSource(vs, vertexShaderSource)
@@ -34,21 +42,40 @@ const fs = gl.createShader(gl.FRAGMENT_SHADER)
 
 const program = gl.createProgram()
 
-gl.attachShader(program, vs);
-gl.attachShader(program, fs);
-
+gl.attachShader(program, vs)
+gl.attachShader(program, fs)
 gl.linkProgram(program)
+
 if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
     console.error('prog info-log:', gl.getProgramInfoLog(program))
     console.error('vert info-log: ', gl.getShaderInfoLog(vs))
     console.error('frag info-log: ', gl.getShaderInfoLog(fs))
 }
+
+fetch('/')
+
+var vertices = new Float32Array([
+    .5, .5,
+    -.5, -.5
+])
+
+var buffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
+
 gl.useProgram(program)
+
+//Passes Vertex positions into vertex shader 'position' attribute
+program.position = gl.getAttribLocation(program, 'position')
+gl.enableVertexAttribArray(program.position)
+gl.vertexAttribPointer(program.position, 2, gl.FLOAT, false, 0, 0)
+
 
 //Backroundcolor
 gl.clearColor(1, 1, 1, 1)
 gl.clear(gl.COLOR_BUFFER_BIT)
 
 //draws points
-gl.drawArrays(gl.POINTS, 0, 1)
+gl.drawArrays(gl.POINTS, 0, vertices.length)
 
