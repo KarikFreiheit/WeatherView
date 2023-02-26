c5876a7a776b8dc915fc594c8e0ec2b549afa6ca
KarikFreiheit <crossoverkarik@gmail.com>
2023-02-25 21:32:39 -0800

    Need to find a way to change positon with velo

--- public/particles.js
+++ public/particles.js
@@ -1,14 +1,13 @@
 let count = 100000;
-const gr = new PIXI.Graphics();
+
 
 var particles = [];
 
 for(let i = 0; i < count; i++){
     let particle = {
-        x: Math.floor(Math.random() * canvas.width),
-        y: Math.floor(Math.random() * canvas.height),
-        radius: 2,
-        velocity: 0,
+        position: PIXI.Vector(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height)),
+        radius: 3,
+        velocity: PIXI.Vector(0,0),
         id: i
     }
     particles.push(particle);
@@ -21,12 +20,23 @@ function Display(particles){
 
     let length = particles.length;
     for(let i = 0; i < length; i++){
-        gr.beginFill();
-        gr.drawCircle(particles[i].x, particles[i].y, particles[i].radius);
-        gr.endFill();
+        let particle = PIXI.Sprite.from("particle.png");
+        particle.position.x = particles[i].x;
+        particle.position.y = particles[i].y;
+        particle.width = particles[i].radius;
+        particle.height = particles[i].radius;
+        particle.anchor.set(0.5);
+        app.stage.addChild(particle);
         //console.log("X: " + particles[i].x + " Y: " + particles[i].y)
     }
-    app.stage.addChild(gr);
 
     console.log("Displayed Particles")
 }
+
+app.ticker.add((delta) => {
+    
+    particles.forEach(function (particle){
+        particle.x +=
+    });
+
+});
